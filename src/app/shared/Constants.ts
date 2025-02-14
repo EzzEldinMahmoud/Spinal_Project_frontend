009a949a32586cfe9094916cc989f8d1a03a8387
EzzEldinMahmoud <72217061+EzzEldinMahmoud@users.noreply.github.com>
2025-02-14 22:12:39 +0200

    Update Constants.ts

--- src/app/shared/Constants.ts
+++ src/app/shared/Constants.ts
@@ -1,4 +1,4 @@
-export const backendEndPoint = "http://localhost:5000/api/";
+export const backendEndPoint = "http://spineproject.runasp.net/api/";
 export const Flask_backendEndPoint = "http://localhost:5500/uploadfile";
 
 export const report_Create_Point = "report/create";//userId x-ray-image
